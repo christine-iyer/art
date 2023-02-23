efdb1c8b579288d07b7743b37e2ff84227c6240d
chrisIyer <hellohotbox420@gmail.com>
2023-02-23 14:02:19 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import BarChart from './components/BarChart'
+import BurrChart from './components/BurrChart';
+import * as d3 from 'd3'
+import data from './utils/data.json'
+import React, { Component } from 'react';
+
+
+
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<BarChart />
+<BurrChart data={data} />
     </div>
   );
 }
